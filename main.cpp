dbf82f5fbe89e9bba48981ad38fc9381073507ae
vikadudu <vicktoriashipkova@yandex.ru>
2025-12-25 01:12:49 +0300

    Добавила проверку

--- main.cpp
+++ main.cpp
@@ -45,9 +45,10 @@ int main() {
     // Шипкова Виктория (блочное шифрование RC6)
     string message1 = "Hello, world!";
     string message2 = "Hello, world! This is my block cipher implementation program! Let's check how it works, I hope it's correct!!!Hello, world! This is my block cipher implementation program! Let's check how it works, I hope it's correct!!!Hello, world! This is my block cipher implementation program! Let's check how it works, I hope it's correct!!!Hello, world! This is my block cipher implementation program! Let's check how it works, I hope it's correct!!!Hello, world! This is my block cipher implementation program! Let's check how it works, I hope it's correct!!!Hello, world! This is my block cipher implementation program! Let's check how it works, I hope it's correct!!!";
-
+    string message3 = "Hello, world!33333333333";
     block_cipher_RC6(message1);
     block_cipher_RC6(message2);
+    block_cipher_RC6(message3);
 
 
     return 0;
